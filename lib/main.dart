38764422151861bcf687d6d1f21fc088dc2a5406
Mehmet Emre Burkay <meburkay@hotmail.com>
2022-05-03 01:31:35 +0300

    async olan main fonksiyonumuzda core paketi vasıtası ile Firebase'i initialize ettim.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../screens/home_page.dart';
@@ -9,7 +10,13 @@ import '../models/mission_data.dart';
 import 'models/my_theme_data.dart';
 
 void main() async {
+//*Eğer runApp'dan önce bir işlem gerçekleştireceksek aşağıdaki komutu yazarak belirtmemiz gerekiyor. Aksi takdirde hata veriyor.
   WidgetsFlutterBinding.ensureInitialized();
+
+//*Firebase'i daha uygulama oluşturulmadan initialize ederek kullanmaya hazır hale getiriyoruz. Bunun için firebase_core paketini import etmemiz gerekiyor. Zaten Firebase özelliklerini kullanmak için öncelikle firebase_core paketini yüklüyoruz.
+  await Firebase.initializeApp();
+
+//*Provider kullanacağım classlardaki oluşturma metodlarını static yaptım. Bu şekilde direk çağırabiliyorum. Aynı şekilde SharedPreferences vasıtası ile alacağım bilgi için oluşturduğum get metodlarını da constructorlar içerisine koydum. Bu şekilde hiçbir hata vermeden hem sp initiliase ediliyor ve ilgili data direk çekiliyor.
   await MyThemeData().spCreate();
   await MissionData().spCreate();
 
